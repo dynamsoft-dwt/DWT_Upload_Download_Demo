a3bc8be29626a1a5c97accd0b5b326e1ad14467e
Tom-Dynamsoft <tom@dynamsoft.com>
2016-08-15 14:55:57 +0800

    adding saving & downloading to & from db (MSSQL, MYSQL, ORACLE)

--- action/php-mysql.php
+++ action/php-mysql.php
@@ -1,12 +1,6 @@
 <?php	
-	$fileTempName = $_FILES['RemoteFile']['tmp_name'];	
-	$fileSize = $_FILES['RemoteFile']['size'];
-	$fileName = $_FILES['RemoteFile']['name'];
-
-	$fReadHandle = fopen($fileTempName, 'rb');
-	$fileContent = fread($fReadHandle, $fileSize);
-    fclose($fReadHandle);
-
+	$strImageID = $_GET["imgID"];
+	
 	// Interacting with MySQL
 	$servername = "127.0.0.1";
 	$username = "root";
@@ -52,12 +46,64 @@
 				die("Error creating table: " . $conn->error);
 			}
 		}			
-		$sql_insertdata = "INSERT INTO ".$tablename." (document_name,document_data) VALUES ('".$fileName."','".addslashes($fileContent)."')";
-        if ($conn->query($sql_insertdata) === TRUE) {
-			// echo "File saved in db successfully.";
-		} else {
-			die("Error saving file: " . $conn->error);
+		if($strImageID == "new"){		
+			$fileTempName = $_FILES['RemoteFile']['tmp_name'];	
+			$fileSize = $_FILES['RemoteFile']['size'];
+			$fileName = $_FILES['RemoteFile']['name'];
+			$strFileSize = (string)intval($fileSize/1024)."KB";
+			$fReadHandle = fopen($fileTempName, 'rb');
+			$fileContent = fread($fReadHandle, $fileSize);
+			fclose($fReadHandle);
+			$imgIndex = 0;
+			$sql_insertdata = "INSERT INTO ".$tablename." (document_name,document_data) VALUES ('".$fileName."','".addslashes($fileContent)."')";
+			if ($conn->query($sql_insertdata) === TRUE) {
+				// echo "File saved in db successfully.";
+				$sql_getIndex = "SELECT id FROM ".$tablename;
+				$IDs = $conn->query($sql_getIndex);
+				if ($IDs->num_rows > 0) {
+					// output data of each row
+					while($row = $IDs->fetch_assoc()) {
+						$_temp = intval($row["id"]);
+						if($_temp > $imgIndex)
+							$imgIndex = $_temp;
+					}
+				}
+			} else {
+				die("Error saving file: " . $conn->error);
+			}
+			$conn->close();
+			echo "PHP:"."DWTUploadFileIndex:".strval($imgIndex)."DWTUploadFileName:".$fileName."UploadedFileSize:".$strFileSize;
+		}
+		else {
+			$sql_getData = "SELECT * FROM ".$tablename." WHERE id = ".$strImageID;
+			$result = $conn->query($sql_getData);
+			if ($result->num_rows > 0) {
+				// output data of each row
+				while($row = $result->fetch_assoc()) {
+					$documentName = $row["document_name"];
+					$strImageExtName = substr($documentName, strlen($documentName) - 3);
+					$documentData = $row["document_data"];
+				}
+			}
+			$conn->close();
+			//Begin writing headers
+			header("Content-Description: File Transfer");
+			$header="Content-Disposition: attachment; filename=".$documentName.";";
+			header($header);
+			header("Content-Transfer-Encoding: binary");
+
+			if($strImageExtName == "bmp"){
+				header('Content-Type: image/bmp');
+			}else if($strImageExtName == "jpg"){
+				header('Content-Type: image/jpg');
+			}else if($strImageExtName == "tif"){
+				header('Content-Type: image/tiff');
+			}else if($strImageExtName == "png"){
+				header('Content-Type: image/png');
+			}else if($strImageExtName == "pdf"){
+				header('Content-Type: application/pdf');
+			}
+			echo $documentData;
 		}
 	}
-	$conn->close();
 ?>
